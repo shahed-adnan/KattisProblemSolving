b5e6b38d03c758667a3ef8e2bce75f93e4e714eb
Shahed <shahedcse.nu@gmail.com>
2023-02-07 11:02:26 +0100

    feat: Added unaccepted code for Dual Visibility

---
+++ DualDivisibility.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+//set<long long int> divisors;
+
+long long int FindDivisors(long long int n, long long int m)
+{
+    long long int cnt=0;
+    for (int i=1; i*i <= n; i++) // O(sqrt(n))
+    {
+        if(n%i==0)
+        {
+            //divisors.insert(i);
+            //divisors.insert(n/i);
+            if(i % m ==0)
+            {
+                cnt++;
+            }
+            if( i != n/i && (n/i) % m ==0)
+            {
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+
+}
+
+/*
+int NumberOfDualDivisibility(long long int m)
+{
+    int cnt=0;
+    for(auto it=divisors.begin(); it != divisors.end(); it++)
+    {
+        if(*it % m ==0)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+*/
+
+int main()
+{
+    long long int n,m;
+    cin>>n>>m;
+    cout<<FindDivisors(n,m)<<endl;
+    //cout<<NumberOfDualDivisibility(m)<<endl;
+
+    return 0;
+}
